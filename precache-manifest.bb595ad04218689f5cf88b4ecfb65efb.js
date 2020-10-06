35e6108baa254b1fe93cdf67b0162376f70eb44c
yunyizhu <yunyizhu@mit.edu>
2020-10-06 00:52:20 -0400

    Updates

---
+++ precache-manifest.bb595ad04218689f5cf88b4ecfb65efb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01d5fe6947fc0c71c28c6e35e97dc1e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4443bf0d3c90a79195f",
+    "url": "/static/css/main.91972687.chunk.css"
+  },
+  {
+    "revision": "96d2e471f4be3318d512",
+    "url": "/static/js/2.3cc0193a.chunk.js"
+  },
+  {
+    "revision": "b4443bf0d3c90a79195f",
+    "url": "/static/js/main.944bc240.chunk.js"
+  },
+  {
+    "revision": "2d904a1f4b536f40b06d",
+    "url": "/static/js/runtime-main.60e7967f.js"
+  },
+  {
+    "revision": "c2cbc58740be7e3d8f5b3957b5082561",
+    "url": "/static/media/profile2.c2cbc587.jpeg"
+  }
+]);
