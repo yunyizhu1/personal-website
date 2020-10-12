8a5c617c0317b4f53b676431174a6e6201e2b671
yunyizhu <yunyizhu@mit.edu>
2020-10-12 15:44:34 -0400

    navbar elements

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ReactComponent as LinkedInIcon } from './icons/linkedin.svg';
 import { ReactComponent as GoogleScholarIcon } from './icons/googlescholar.svg';
+import { ReactComponent as GithubIcon } from './icons/github.svg';
 
 const Navbar = ( {scrollFunction} ) => {
   return (
     <nav>
       <div className="navigation">
-        <Link to="/" className="name-brand">Yunyi Zhu</Link>
+        <Link to="/" onClick={() => window.location.reload()} className="name-brand">Yunyi Zhu</Link>
         <div className="menu">
           <button onClick={() => scrollFunction("research")}>Publications</button>
           <button onClick={() => scrollFunction("awards")}>Awards</button>
@@ -15,10 +16,19 @@ const Navbar = ( {scrollFunction} ) => {
       </div>
       <div className="info">
         <div className="googlescholar">
-          <a href="https://www.linkedin.com/in/yunyizhu97/" target="_blank"><GoogleScholarIcon /></a>
+          <a href="https://scholar.google.com/citations?user=tExDwdkAAAAJ&hl=en" target="_blank">
+            <GoogleScholarIcon />
+          </a>
+        </div>
+        <div className="github">
+          <a href="https://github.com/yunyizhu1" target="_blank">
+            <GithubIcon />
+          </a>
         </div>
         <div className="linkedin">
-          <a href="https://www.linkedin.com/in/yunyizhu97/" target="_blank"><LinkedInIcon /></a>
+          <a href="https://www.linkedin.com/in/yunyizhu97/" target="_blank">
+            <LinkedInIcon />
+          </a>
         </div>
       </div>
     </nav>
