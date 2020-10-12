264b89d0270f28cdf6ff420cecc8a08f4abf50a9
yunyizhu <yunyizhu@mit.edu>
2020-10-12 01:35:10 -0400

    added links to publication

--- src/components/research/research_preview.js
+++ src/components/research/research_preview.js
@@ -6,14 +6,22 @@ const Research = ({research}) => (
         <img src={require("../../data/research_thumbnails/" + research.thumbnail) } />
       </div>
       <div className="descriptions">
-        <div className="title">
-          {research.title}
+        <div>
+          <div className="title">
+            {research.title}
+          </div>
+          <div className="author">
+            {research.authors}
+          </div>
+          <div className="conference">
+            in <div>{research.conference}</div>
+          </div>
         </div>
-        <div className="author">
-          {research.authors}
-        </div>
-        <div className="conference">
-          in <div>{research.conference}</div>
+        <div className="links">
+          {!!research.doi && <a href={research.doi} target="_blank">DOI</a>}
+          {!!research.pdf && <a href={research.pdf} target="_blank">PDF</a>}
+          {!!research.video && <a href={research.video} target="_blank">Video</a>}
+          {!!research.projectPage && <a href={research.projectPage} target="_blank">Project Page</a>}
         </div>
       </div>
     </div>
